07ff3ff4ec724abdd88f635b3111370fb727f390
celisawalker <celisa.walker@gmail.com>
2025-05-05 23:01:16 -0700

    updated css for index and base templates

--- concertlogger/concertlogger/settings.py
+++ concertlogger/concertlogger/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h&#7dus^w%v*uxl@bg+a7-!pfj!h_u6_1p+!o7s=^z_25i$y3o'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
