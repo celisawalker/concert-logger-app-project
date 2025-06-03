709f1becc34db95e46b086946f9301d7d83820c6
celisawalker <celisa.walker@gmail.com>
2025-06-02 21:47:19 -0700

    made more tiny changes

--- concertlogger/settings.py
+++ concertlogger/settings.py
@@ -33,7 +33,7 @@ import json
 
 # Set configuration parameter: return "https" URLs by setting secure=True  
 # ==============================
-config = cloudinary.config(secure=True)
+
 
 # Log the configuration
 # ==============================
@@ -177,4 +177,5 @@ cloudinary.config(
     cloud_name = os.getenv('cloud_name'),
     api_key = os.getenv('api_key'),
     api_secret = os.getenv('api_secret'),
+    secure = True
 )
